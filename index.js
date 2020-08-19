a459dc75e8ee4a4372b90cebcc2bec07cf8572f1
Parker Williamson <parker.williamson@gmail.com>
2020-08-19 20:25:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = array => {
+  let winner = array.find( game => game.result === 'W')
+  if (winner) {
+    return winner.year
+  }else
+  return undefined
+}
